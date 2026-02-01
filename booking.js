5c1d90b4d562cc472bf09d70594dbf2abf4ca8de
MUZAMMIL ASIF <masif712@gmial.com>
2026-02-01 08:20:44 +0500

    Initial commit: booking page files

---
+++ booking.js
@@ -0,0 +1,10 @@
+let Stay = true;
+if (Stay == true) {
+  document.querySelector("monitor").innerHTML =
+    " Find your next stay" +
+    "Search low prices on hotels, homes and much more...";
+} else {
+  document.querySelector("monitor").innerHTML =
+    " Car hire for any kind of trip" +
+    "Great cars at great prices, from the biggest car rental companies";
+}
